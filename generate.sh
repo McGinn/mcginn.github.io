b1a95b8c74305d10792d04e1ad05762c62aaf213
McGinn <alexander.mcginn@gmail.com>
2017-03-10 00:18:03 -0500

    verbose publish

--- generate.sh
+++ generate.sh
@@ -40,7 +40,7 @@ generate_site() {
     git config --global user.name "Travis CI"
   else
     cd "$rootPath" || exit 1
-    pelican -s $PUBLISH_CONF
+    pelican -s $PUBLISH_CONF -v
   fi
 
   # Pull hash and commit message of the most recent commit

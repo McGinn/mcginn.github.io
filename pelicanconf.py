963e0c1136a8d0fa5dde420c9c1c47e12be8eec4
McGinn <alexander.mcginn@gmail.com>
2017-03-10 12:23:02 -0500

    commented out header info in pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+'''
+###!/usr/bin/env python
+### -*- coding: utf-8 -*- #
+###from __future__ import unicode_literals
+'''
 
 # Theme-specific settings
 SITENAME = 'Alexander McGinn'
